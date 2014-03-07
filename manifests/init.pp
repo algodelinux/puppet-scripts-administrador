b2e993ddf88ce8e53908801982c1b669cf87d3ea
Esteban M. Navas Martín <algodelinux@gmail.com>
2014-03-07 09:40:38 +0100

    Import inicial del módulo puppet-scripts-administrador

---
+++ manifests/init.pp
@@ -0,0 +1,40 @@
+class scripts-administrador {
+
+   case $use {
+        "portatil-profesor-2011": {
+           file { "/usr/local/sbin":
+              source=>"puppet://puppetinstituto/scripts-administrador/portatil-profesor",
+              recurse => true
+           }
+         }
+        "portatil-alumno-2011": {
+           file { "/usr/local/sbin":
+              source=>"puppet://puppetinstituto/scripts-administrador/portatil-alumno",
+              recurse => true
+           }
+        }
+        "ltsp-server": {
+           file { "/usr/local/sbin":
+              source => "puppet://puppetinstituto/scripts-administrador/ltsp-server",
+              recurse => true
+           }
+           
+           cron { "limpieza-ltsp":
+	      command => "/usr/local/sbin/cleantmpinactiveusers",
+	      ensure => present,
+              hour => [8-15],
+	      minute => "*/5",
+	      weekday => [1-5]
+	   }	      
+        }
+        "workstation": {
+           file { "/usr/local/sbin":
+              source=>"puppet://puppetinstituto/scripts-administrador/workstation",
+              recurse => true
+           }
+
+        }
+        default: { }
+   }
+
+}
