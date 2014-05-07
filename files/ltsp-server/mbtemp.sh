63a278ae51548634719aec6fd69536c3067c469c
Esteban M. Navas Martín <algodelinux@gmail.com>
2014-05-07 20:06:38 +0200

    Update mbtemp.sh

--- files/ltsp-server/mbtemp.sh
+++ files/ltsp-server/mbtemp.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 MBTEMP=`sensors |grep "MB Temperature"| cut -f7 -d" " | tr -d '+' | tr -d '°C'  | awk '{printf("%d\n",$1 + 0.5)}'`
-echo $MBTEMP # for debug only
-
+# echo $MBTEMP # for debug only
 exit $MBTEMP
 
