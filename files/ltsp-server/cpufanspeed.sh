78a02e63daf058139a65f4be6adfa72d81c1cff0
Esteban M. Navas Martín <algodelinux@gmail.com>
2014-05-07 20:07:02 +0200

    Update cpufanspeed.sh

--- files/ltsp-server/cpufanspeed.sh
+++ files/ltsp-server/cpufanspeed.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 CPUFANSPEED=`sensors |grep "CPU FAN Speed"| cut -f8 -d" " | awk '{printf("%d\n",$1 + 0.5)}'`
-echo $CPUFANSPEED # for debug only
-
+# echo $CPUFANSPEED # for debug only
 exit $CPUFANSPEED
 
