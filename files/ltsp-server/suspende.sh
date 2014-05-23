9c486d42e187dbd99ea404d0f493bec1ef87219d
Esteban M. Navas Martín <algodelinux@gmail.com>
2014-05-23 17:13:10 +0200

    Update suspende.sh
    Añadiendo condición que lance el script de apagado de clientes sólo si es un aula de thinclients o mixta.

--- files/ltsp-server/suspende.sh
+++ files/ltsp-server/suspende.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Esteban M. Navas
 # IES Valle del Jerte - Plasencia
-# 20/05/2014
+# Fecha creación: 20/05/2014
+# Fecha última modificación: 23/05/2014
 
 # suspende.sh -> Apaga los terminales de un aula y suspende el equipo. Para apagar los terminales 
 # utilizamos el script /usr/sbin/apagaterminales.sh y la herramienta pm-suspend que se encuentra instalada en los 
@@ -9,10 +10,17 @@
 #
 # La idea es utilizar este script como tarea cron de forma que se ejecute cada cierto tiempo
 
-# Si hay un usuario logueado en el sistema, finalizamos
+# Si hay un usuario logueado en el terminal :0, no hacemos nada y finalizamos
 if [ $(/usr/bin/w|/bin/grep ":0" | /usr/bin/cut -f1 -d" ") ]; then
    exit 0
 fi
 
-# Si no hay un usuario logueado, apagamos los terminales y suspendemos el equipo
-/usr/sbin/apagaterminales.sh && /usr/sbin/pm-suspend &
+clientes=`facter clients`
+
+# Si no hay un usuario logueado, apagamos los terminales
+if [ $clientes = "all" ] || [ $clientes = "thinclients" ]; then
+   /usr/sbin/apagaterminales.sh
+fi
+
+# Si no hay un usuario logueado en el terminal :0 suspendemos el equipo
+/usr/sbin/pm-suspend &
