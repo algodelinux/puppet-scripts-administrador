bb348d7ebf815a85b154ce77799b0a106d5c9712
algodelinux <algodelinux@gmail.com>
2017-10-04 12:57:58 +0200

    Upgraded module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,10 @@
 class puppet-scripts-administrador {
 
    case $use {
-        "portatil-profesor-2011": {
-           file { "/usr/local/sbin":
-              source=>"puppet://puppetinstituto/puppet-scripts-administrador/portatil-profesor",
-              recurse => true
-           }
-         }
-        "portatil-alumno-2011": {
-           file { "/usr/local/sbin":
-              source=>"puppet://puppetinstituto/puppet-scripts-administrador/portatil-alumno",
-              recurse => true
-           }
-        }
         "ltsp-server": {
            file { "/usr/local/sbin":
-              source => "puppet://puppetinstituto/puppet-scripts-administrador/ltsp-server",
+              source => "puppet:///modules/puppet-scripts-administrador/ltsp-server",
+              owner => root, group => root,		
               recurse => true
            }
            
@@ -27,12 +16,44 @@ class puppet-scripts-administrador {
 	      weekday => [1-5]
 	   }	      
         }
-        "workstation": {
-           file { "/usr/local/sbin":
-              source=>"puppet://puppetinstituto/puppet-scripts-administrador/workstation",
-              recurse => true
-           }
+        "ubuntu": {
+           case $tipo {
+              "notebookACER","notebookAPD","notebookXTREM","notebookHP": {
+                 file { "/usr/local/sbin":
+                    source=>"puppet:///modules/puppet-scripts-administrador/$tipo",
+                    owner => root, group => root,		 
+                    recurse => true
+                 }
+              }  
 
+              "workstation": {
+                 file { "/usr/local/sbin":
+                    source=>"puppet:///modules/puppet-scripts-administrador/workstation",
+                    owner => root, group => root,		
+                    recurse => true
+                 }
+              }
+
+              "siatic": {
+                 file { "/usr/local/sbin/reinstallnvidia":
+                    source => "puppet:///modules/puppet-scripts-administrador/siatic/reinstallnvidia",
+                    owner => root, group => root, mode => 755
+                 }       
+                 file { "/usr/local/sbin/reconfigurenvidia":
+                    source => "puppet:///modules/puppet-scripts-administrador/siatic/reconfigurenvidia",
+                    owner => root, group => root, mode => 755
+                 }       
+              }
+
+              "Telefonica": {
+                 file { "/usr/local/sbin/reinstall_nwfermi_module":
+                    source => "puppet:///modules/puppet-scripts-administrador/Telefonica/reinstall_nwfermi_module",
+                    owner => root, group => root, mode => 755
+                 }       
+              }
+              
+              default: { }
+           }
         }
         default: { }
    }
